7030aac5650cf872049811a642f559d1d911f7c2
Yasin Emre Karabacak <yasinemre453@gmail.com>
2024-10-26 20:58:15 +0300

    Add project files.

---
+++ colorChanger/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace colorChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorButton1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // colorButton1
+            // 
+            this.colorButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.colorButton1.Location = new System.Drawing.Point(342, 319);
+            this.colorButton1.Name = "colorButton1";
+            this.colorButton1.Size = new System.Drawing.Size(99, 63);
+            this.colorButton1.TabIndex = 0;
+            this.colorButton1.Text = "Change";
+            this.colorButton1.UseVisualStyleBackColor = true;
+            this.colorButton1.Click += new System.EventHandler(this.colorButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.colorButton1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Color Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colorButton1;
+    }
+}
+
---
+++ colorChanger/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colorChanger
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            colorButton1.Click += new EventHandler(this.colorButton1_Click);
+            this.Controls.Add(colorButton1);
+        }
+
+        private void colorButton1_Click(object sender, EventArgs e)
+        {
+             int red = random.Next(0,256);
+             int green = random.Next(0,256);
+             int blue = random.Next(0,256);
+
+            this.BackColor = Color.FromArgb(red, green, blue);
+        }
+    }
+}
